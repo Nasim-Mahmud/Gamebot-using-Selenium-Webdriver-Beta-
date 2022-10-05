45a7626cb25ac8d665b0cb1d366b7c17bf0eea4e
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-10-06 05:41:08 +0600

    Initializing!

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+from selenium import webdriver
+
 chrome_drive_path = "C:\Development\chromedriver.exe"
+driver = webdriver.Chrome(executable_path=chrome_drive_path)
+
+driver.get("https://www.amazon.com")
