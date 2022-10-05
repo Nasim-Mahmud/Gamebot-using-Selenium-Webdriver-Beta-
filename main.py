50be744f81259de021251863889b31ba8b208653
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-10-06 05:57:48 +0600

    Initializing!

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
 chrome_drive_path = "C:\Development\chromedriver.exe"
-driver = webdriver.Chrome(executable_path=chrome_drive_path)
+service_obj = Service(executable_path=chrome_drive_path)
+driver = webdriver.Chrome(service=service_obj)
 
 driver.get("https://www.amazon.com")
+# driver.close()
