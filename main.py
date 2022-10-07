7ee9f680e4a7861731866ca10ebd02b952bbd293
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-10-07 08:47:23 +0600

    Initializing!

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 chrome_drive_path = "C:\Development\chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chrome_drive_path)
@@ -9,10 +10,13 @@ driver.get("https://www.amazon.com/FIODIO-Comfortable-"
            "rd_w=Cpi3v&pd_rd_wg=YqINu&pf_rd_p=12129333-2117-4490-9c17-6d31baf0582a&pf_rd_r=F5"
            "4CV71S6YMFKCMAKY66&qid=1665017179&qu=eyJxc2MiOiI3LjE0IiwicXNhIjoiNi44MCIsInFzcCI6IjYu"
            "MjEifQ%3D%3D&sr=8-1-spons&psc=1")
-price = driver.switch_to.__class__
-print(price.text)
 
-
-
+# Comment: Class a-offscreen not working!
+price1 = driver.find_element(By.CLASS_NAME, "a-price-whole").text
+print(price1)
+price2 = driver.find_element(By.CLASS_NAME, "a-price-fraction").text
+print(price2)
 
 driver.quit()
+price = price1 + "." + price2
+print(price)
