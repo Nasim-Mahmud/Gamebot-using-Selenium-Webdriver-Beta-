13595c6f2fafa02ba96237c4765356a1ebdf4116
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-10-08 11:40:25 +0600

    Initializing!

--- main.py
+++ main.py
@@ -2,19 +2,25 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
+LINK = "https://www.python.org/"
 
 # Removing deprecationWarning: executable_path has been deprecated
 chrome_drive_path = "C:\Development\chromedriver.exe"
 s = Service(chrome_drive_path)
 driver = webdriver.Chrome(service=s)
-driver.get("https://selenium-python.readthedocs.io/")
+driver.get(LINK)
 
-# Comment: Class a-offscreen not working!
-price1 = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div/div[1]/h3").text
-print(price1)
-# price2 = driver.find_element(By.CLASS_NAME, "a-price-fraction").text
-# print(price2)
-#
+event_times = driver.find_elements(By.CSS_SELECTOR, ".event-widget time")
+event_dates = driver.find_elements(By.CSS_SELECTOR, ".event-widget .menu a")
+
+events = {}
+for n in range(len(event_times)):
+    events[n] = {
+        "time": event_times[n].text,
+        "name": event_dates[n].text
+    }
+
+print(events)
+
 driver.quit()
-# price = price1 + "." + price2
-# print(price)
+
