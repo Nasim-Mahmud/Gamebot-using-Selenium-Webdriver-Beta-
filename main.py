d34779252eab850c9ab1fd3a98cc1c6c2a901ca2
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-10-09 11:10:07 +0600

    Initializing!

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
-LINK = "https://www.python.org/"
+LINK = "https://orteil.dashnet.org/cookieclicker/"
 
 # Removing deprecationWarning: executable_path has been deprecated
 chrome_drive_path = "C:\Development\chromedriver.exe"
@@ -10,18 +10,13 @@ s = Service(chrome_drive_path)
 driver = webdriver.Chrome(service=s)
 driver.get(LINK)
 
-event_times = driver.find_elements(By.CSS_SELECTOR, ".event-widget time")
-event_dates = driver.find_elements(By.CSS_SELECTOR, ".event-widget .menu a")
-
-events = {}
-for n in range(len(event_times)):
-    events[n] = {
-        "time": event_times[n].text,
-        "name": event_dates[n].text
-    }
-
-print(events)
+terms = driver.find_element(By.CLASS_NAME, 'cc_container--open cc_btn cc_btn_accept_all')
+terms.click()
+lang = driver.find_element(By.XPATH, '//*[@id="langSelect-EN"]')
+lang.click()
+# cookie = driver.find_element(By.ID, "bigCookie")
+# cookie.click()
 
 
-driver.quit()
+# driver.quit()
 
