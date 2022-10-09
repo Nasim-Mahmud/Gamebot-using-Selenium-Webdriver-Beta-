3e398364416b7a4dd5ddfe23e31dbdbd27652d1c
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-10-09 11:40:47 +0600

    Clicking cookie!

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 
 LINK = "https://orteil.dashnet.org/cookieclicker/"
@@ -10,13 +13,15 @@ s = Service(chrome_drive_path)
 driver = webdriver.Chrome(service=s)
 driver.get(LINK)
 
-terms = driver.find_element(By.CLASS_NAME, 'cc_container--open cc_btn cc_btn_accept_all')
-terms.click()
-lang = driver.find_element(By.XPATH, '//*[@id="langSelect-EN"]')
-lang.click()
-# cookie = driver.find_element(By.ID, "bigCookie")
-# cookie.click()
 
+actions = ActionChains(driver)
+driver.implicitly_wait(10)
+language = driver.find_element(By.ID, "langSelect-EN").click()
+driver.implicitly_wait(50)
+terms = driver.find_element(By.XPATH, '/html/body/div[1]/div/a[1]').click()
+
+for n in range(5000):
+    cookie = driver.find_element(By.ID, "bigCookie").click()
 
 # driver.quit()
 
