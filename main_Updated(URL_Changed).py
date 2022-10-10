152791232030637406439dc3d177d74966130538
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-10-10 21:08:45 +0600

    Initializing Part 02!

--- main_Updated(URL_Changed).py
+++ main_Updated(URL_Changed).py
@@ -2,10 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
-LINK = "https://orteil.dashnet.org/cookieclicker/"
+LINK = "http://orteil.dashnet.org/experiments/cookie/"
 
 chrome_drive_path = "C:\Development\chromedriver.exe"
 s = Service(chrome_drive_path)
 driver = webdriver.Chrome(service=s)
 driver.get(LINK)
 
+driver.implicitly_wait(50)
+driver.find_element(By.XPATH, '//*[@id="whole"]/div[1]/div/a[1]').click()
+
+game_on = True
+while game_on:
+    cookie = driver.find_element(By.XPATH, '//*[@id="cookie"]')
+    cookie.click()
