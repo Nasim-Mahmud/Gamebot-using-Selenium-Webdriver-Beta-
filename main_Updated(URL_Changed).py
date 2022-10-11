524a1021f349b8e85db2beafb8910228b07b8a94
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-10-11 12:05:05 +0600

    Initializing Part 02!

--- main_Updated(URL_Changed).py
+++ main_Updated(URL_Changed).py
@@ -10,19 +10,5 @@ driver = webdriver.Chrome(service=s)
 driver.get(LINK)
 
 driver.implicitly_wait(50)
-driver.find_element(By.XPATH, '//*[@id="whole"]/div[1]/div/a[1]').click()
 
-game_on = True
-cookie = driver.find_element(By.XPATH, '//*[@id="cookie"]')
-cursor_price = driver.find_element(By.XPATH, '//*[@id="buyCursor"]/b').text.split(" ")[2]
-cursor = driver.find_element(By.XPATH, '//*[@id="buyCursor"]')
-
-
-for n in range(20):
-    cookie_number = driver.find_element(By.ID, "money").text
-    if int(cookie_number) > int(cursor_price):
-        cursor.click()
-    cookie.click()
-
-print(driver.find_element(By.XPATH, '//*[@id="buyCursor"]/b').text.split(" "))
 driver.quit()
