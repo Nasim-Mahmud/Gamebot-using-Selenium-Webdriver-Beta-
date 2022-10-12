70befe754b4ffe052d5bdb5130aa33b913fd3184
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-10-12 11:03:45 +0600

    Initializing Part 02!

--- main_Updated(URL_Changed).py
+++ main_Updated(URL_Changed).py
@@ -9,7 +9,6 @@ s = Service(chrome_drive_path)
 driver = webdriver.Chrome(service=s)
 driver.get(LINK)
 
-
 # driver.implicitly_wait(50)
 # Cursor price
 def cursor_price():
@@ -19,43 +18,43 @@ def cursor_price():
 
 # Grandma price
 def grandma_price():
-    gp = driver.find_element(By.XPATH, '//*[@id="buyCursor"]/b').text.split(" ")[2]
+    gp = driver.find_element(By.XPATH, '//*[@id="buyGrandma"]/b').text.split(" ")[2]
     return int(gp)
 
 
 # Factory price
 def factory_price():
-    fp = driver.find_element(By.XPATH, '//*[@id="buyCursor"]/b').text.split(" ")[2]
+    fp = driver.find_element(By.XPATH, '//*[@id="buyFactory"]/b').text.split(" ")[2]
     return int(fp)
 
 
 # Mine price
 def mine_price():
-    mp = driver.find_element(By.XPATH, '//*[@id="buyCursor"]/b').text.split(" ")[2]
+    mp = driver.find_element(By.XPATH, '//*[@id="buyMine"]/b').text.split(" ")[2]
     return int(mp)
 
 
 # Shipment price
 def shipment_price():
-    sp = driver.find_element(By.XPATH, '//*[@id="buyCursor"]/b').text.split(" ")[2]
+    sp = driver.find_element(By.XPATH, '//*[@id="buyShipment"]/b').text.split(" ")[2]
     return int(sp)
 
 
 # Alchemy price
 def alchemy_price():
-    ap = driver.find_element(By.XPATH, '//*[@id="buyCursor"]/b').text.split(" ")[2]
+    ap = driver.find_element(By.XPATH, '//*[@id="buyAlchemy lab"]/b').text.split(" ")[3]
     return int(ap)
 
 
 # Portal price
 def portal_price():
-    pp = driver.find_element(By.XPATH, '//*[@id="buyCursor"]/b').text.split(" ")[2]
+    pp = driver.find_element(By.XPATH, '//*[@id="buyPortal"]/b').text.split(" ")[2]
     return int(pp)
 
 
 # Time Machine price
 def time_machine_price():
-    tmp = driver.find_element(By.XPATH, '//*[@id="buyCursor"]/b').text.split(" ")[2]
+    tmp = driver.find_element(By.XPATH, '//*[@id="buyTime machine"]/b').text.split(" ")[3]
     return int(tmp)
 
 
@@ -69,7 +68,12 @@ pp = portal_price()
 tmp = time_machine_price()
 
 print(cp)
-# print(ap)
-# print(tmp)
+print(gp)
+print(fp)
+print(mp)
+print(sp)
+print(ap)
+print(pp)
+print(tmp)
 
 driver.quit()
